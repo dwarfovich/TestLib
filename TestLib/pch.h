4d971103313eff4a0b8eae0cebb632c79829d791
Tim Gilevich <ouncetim@gmail.com>
2023-10-27 14:19:58 +0300

    WIP

--- TestLib/pch.h
+++ TestLib/pch.h
@@ -5,41 +5,8 @@
 
 #include "framework.h"
 #include "TString.h"
-// #include "PathUtils.h"
-// #include "StringUtils.h"
-
 #include "nlohmann/json.hpp"
-#include "curl/curl.h"
-
-#include <Windows.h>
-#include <tchar.h>
-#include <afxwin.h>
-#include <afxcmn.h>
-#include <atlstr.h>
-#include <shellapi.h>
-#include <Urlmon.h>
-#include <wininet.h>
-#include <conio.h>
-#include <gdiplus.h>
-#include <atlimage.h>
-
-// Needed to fix cUrl linking error.
-// See
-// https://stackoverflow.com/questions/30450042/unresolved-external-symbol-imp-iob-func-referenced-in-function-openssldie
-// & https://stackoverflow.com/questions/32418766/c-unresolved-external-symbol-sprintf-and-sscanf-in-visual-studio-2015.
-#pragma comment(lib, "legacy_stdio_definitions.lib")
-#include <iostream>
-namespace details {
-FILE _iob[] = { *stdin, *stdout, *stderr };
-}
-extern "C" FILE* __cdecl __iob_func(void)
-{
-    return details::_iob;
-}
 
 #include <string>
-#include <algorithm>
-#include <mutex>
-#include <unordered_set>
 
 #endif // PCH_H
